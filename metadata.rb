6bf8da6e4d4ef47e313153b3c950fd31103a33e6
root <root@ip-172-31-16-130.us-west-2.compute.internal>
2017-03-03 18:37:51 +0000

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'install_mongo'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures install_mongo'
+long_description 'Installs/Configures install_mongo'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: install_mongo
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
