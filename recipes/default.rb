d03b0e6c01377c5b03912856ff2adf6d0bf1d40c
root <root@ip-172-31-16-130.us-west-2.compute.internal>
2017-03-05 02:59:37 +0000

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,17 @@
-#
-# Cookbook Name:: install_mongo
-# Recipe:: default
-#
-# Copyright (c) 2017 The Authors, All Rights Reserved.
+template "/etc/yum.repos.d/mongodb.repo"  do
+  variables(
+    :baseurl => "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/"
+  )
+end
+
+# Install the MongoDB package.
+package "mongodb-org" do
+  action :install
+end
+
+# Start MongoDB.
+# ensure that MongoDB will start following a system reboot
+service "mongod" do
+  action [:start, :enable]
+end
+
